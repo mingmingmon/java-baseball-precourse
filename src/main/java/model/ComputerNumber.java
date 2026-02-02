36e79ac9ed5a202f9e0898dde514e3f7ef31ca6e
mingmingmon <mingmingmon@naver.com>
2026-02-03 00:45:46 +0900

    feat(random number): 봇의 숫자 생성 로직 구현

---
+++ src/main/java/model/ComputerNumber.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ComputerNumber {
+
+	private static final int SIZE = 3;
+	private static final int MIN = 1;
+	private static final int MAX = 9;
+
+	private final List<Integer> numbers;
+
+	public ComputerNumber() {
+		this.numbers = generate();
+	}
+
+	private List<Integer> generate() {
+		List<Integer> pool = new ArrayList<>();
+		for (int i = MIN; i <= MAX; i++) {
+			pool.add(i);
+		}
+
+		Collections.shuffle(pool);
+		return pool.subList(0, SIZE);
+	}
+
+	public List<Integer> getNumbers() {
+		return numbers;
+	}
+
+}
