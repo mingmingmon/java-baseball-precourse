a1a55d1b79c0c70d7b49a8dcb4290e850358ab93
mingmingmon <mingmingmon@naver.com>
2026-02-04 23:09:13 +0900

    fix(ball): 플레이어가 입력으로 중복된 숫자를 입력한 경우를 고려하여 볼 카운팅 로직 수정

--- src/main/java/model/BallCounter.java
+++ src/main/java/model/BallCounter.java
@@ -1,15 +1,18 @@
 package model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BallCounter {
 
 	public int count(ComputerNumber computerNumber, PlayerNumber playerNumber) {
 		List<Integer> computer = computerNumber.getNumbers();
 		List<Integer> player = playerNumber.getNumbers();
+		Set<Integer> playerSet = new HashSet<>(player);
 
 		int ball = 0;
-		for (int i = 0; i < player.size(); i++) {
+		for (int i = 0; i < playerSet.size(); i++) {
 			int playerNumberAt = player.get(i);
 
 			if (computer.contains(playerNumberAt)
