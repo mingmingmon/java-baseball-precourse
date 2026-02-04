d0ec6bf9d59af569498afa7f79feffe7f1519a1d
mingmingmon <mingmingmon@naver.com>
2026-02-04 23:11:33 +0900

    test(ball): 볼 개수 카운팅 로직 단위 테스트

---
+++ src/test/java/model/BallCounterTest.java
@@ -0,0 +1,53 @@
+package model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import model.object.TestComputerNumber;
+
+public class BallCounterTest {
+
+	private final BallCounter ballCounter = new BallCounter();
+
+	@Test
+	@DisplayName("같은 숫자가 다른 위치에 있으면 볼로 계산한다")
+	void countBallWhenNumberMatchesButPositionDiffers() {
+		ComputerNumber computer = TestComputerNumber.of(1, 2, 3);
+		PlayerNumber player = new PlayerNumber("321");
+
+		int ball = ballCounter.count(computer, player);
+		assertThat(ball).isEqualTo(2);
+	}
+
+	@Test
+	@DisplayName("같은 위치의 숫자는 볼로 계산하지 않는다")
+	void doNotCountBallWhenPositionMatches() {
+		ComputerNumber computer = TestComputerNumber.of(1, 2, 3);
+		PlayerNumber player = new PlayerNumber("129");
+
+		int ball = ballCounter.count(computer, player);
+		assertThat(ball).isEqualTo(0);
+	}
+
+	@Test
+	@DisplayName("플레이어의 숫자에서 중복된 수가 있는 경우 스트라이크를 볼보다 우선시 한다")
+	void countBallOnceEvenIfPlayerHasDuplicates() {
+		ComputerNumber computer = TestComputerNumber.of(1, 2, 3);
+		PlayerNumber player = new PlayerNumber("121");
+
+		int ball = ballCounter.count(computer, player);
+		assertThat(ball).isEqualTo(0);
+	}
+
+	@Test
+	@DisplayName("스트라이크로 사용된 숫자는 볼로 계산되지 않는다")
+	void doNotCountBallForStrikeNumbers() {
+		ComputerNumber computer = TestComputerNumber.of(1, 2, 3);
+		PlayerNumber player = new PlayerNumber("131");
+
+		int ball = ballCounter.count(computer, player);
+		assertThat(ball).isEqualTo(1);
+	}
+}
