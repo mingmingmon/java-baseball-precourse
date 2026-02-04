47e40d3360f5a79d856a8b8e13fa439ebfa88906
mingmingmon <mingmingmon@naver.com>
2026-02-04 23:58:36 +0900

    feat(view): 게임 진행 및 종료 뷰 구현

---
+++ src/main/java/view/GameView.java
@@ -0,0 +1,32 @@
+package view;
+
+import java.util.Scanner;
+
+import model.object.Result;
+
+public class GameView {
+
+	private final Scanner scanner = new Scanner(System.in);
+
+	public String readPlayerNumber() {
+		System.out.print("숫자를 입력해주세요 : ");
+		return scanner.nextLine();
+	}
+
+	public void printResult(Result result) {
+		System.out.println(result.toString());
+	}
+
+	public void printGamClear() {
+		System.out.println("3개의 숫자를 모두 맞히셨습니다!");
+	}
+
+	public int readRestartCommand() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		return Integer.parseInt(scanner.nextLine());
+	}
+
+	public void printErrorMessage(String message) {
+		System.out.println(message);
+	}
+}
